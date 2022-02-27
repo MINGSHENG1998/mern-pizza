4a61b950748fbe980b32ad895c51fa80243317a6
mingsheng <khawmingsheng@altecflex.com>
2022-02-28 01:17:47 +0800

    added modal for pizza

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
 
 export default function Pizza({ pizzas }) {
   const { quantity, setQuantity } = useState(1);
   const { varient, setVarient } = useState("small");
+  const { show, setShow } = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <div style={{ margin: "70px" }} className=" shadow-lg p-3 mb-5 bg-body rounded">
-      <h1>{pizzas.name}</h1>
-      <img src={pizza.image} className="img-fluid pizza_img" />
+    <div
+      style={{ margin: "70px" }}
+      className=" shadow-lg p-3 mb-5 bg-body rounded"
+    >
+      <div onClick={handleShow}>
+        <h1>{pizzas.name}</h1>
+        <img src={pizza.image} className="img-fluid pizza_img" />
+      </div>
       <div className="flex-container">
         <div className="m-1 w-100">
           <p>Varients</p>
@@ -40,12 +50,31 @@ export default function Pizza({ pizzas }) {
       </div>
       <div className="flex-container">
         <div className="m-1 w-100">
-          <h1 className="mt-1">Price: RM {pizza.prices[0][varient] * quantity}</h1>
+          <h1 className="mt-1">
+            Price: RM {pizza.prices[0][varient] * quantity}
+          </h1>
         </div>
         <div className="m-1 w-100">
           <button className="pizza_btn">ADD TO CART</button>
         </div>
       </div>
+
+      {/* Modal */}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{pizza.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img src={pizza.image} className="img-fluid pizza_img_l" />
+          <p>{pizza.description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="pizza_btn" onClick={handleClose}>
+            CLOSE
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
