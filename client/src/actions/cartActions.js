7ee7ed77d909cba76663f8a33d5af9d42f86f63b
mingsheng <khawmingsheng@altecflex.com>
2022-03-01 00:24:08 +0800

    fixed negative quantity bug

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -12,10 +12,12 @@ export const addToCart = (pizza, quantity, varient) => (dispatch, getState) => {
   if (cartItem.quantity > 10) {
     alert("You cannot add more than 10 quantities");
   } else {
-    if (cartItem.quantity < 0) {
+    if (cartItem.quantity < 1) {
       dispatch({ type: "DELETE_FROM_CART", payload: pizza });
     }
-    dispatch({ type: "ADD_TO_CART", payload: cartItem });
+    else{
+      dispatch({ type: "ADD_TO_CART", payload: cartItem });
+    }    
   }
 
   const cartItems = getState().cartReducer.cartItems;
